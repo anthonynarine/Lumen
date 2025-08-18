f30f140029008291852995bea3cff7855cde3564
mitrasingh <mitra.coder@gmail.com>
2025-08-17 22:34:06 -0400

    feat: add protected route component for authenticated route protection

---
+++ frontend/src/auth/utils/ProtectedRoute.tsx
@@ -0,0 +1,18 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../../auth/hooks/useAuth";
+
+type ProtectedRouteProps = {
+	children: React.ReactNode;
+};
+
+const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
+	const { state } = useAuth();
+
+	if (!state) {
+		return <Navigate to="/login" replace />;
+	}
+
+	return <>{children}</>;
+};
+
+export default ProtectedRoute;
