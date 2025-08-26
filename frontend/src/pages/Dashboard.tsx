ad8be3cc5148135173258ee23cdd919e250486f0
mitrasingh <mitra.coder@gmail.com>
2025-08-25 20:53:00 -0400

    refactor: remove CSS class from h1 node - causes bug when theme is changed"

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -2,13 +2,11 @@ import React from "react";
 import PageContainer from "../layout/MainContent/PageContainer";
 
 export default function Dashboard() {
-  return (
-  <PageContainer>
-    <h1 className="text-2xl font-bold text-gray-900 dark:text-white mb-4 mt-10 ml-35">
-      Dashboard
-    </h1>
-    <p>test</p>
-    {/* dashboard content */}
-  </PageContainer>
-  );
+	return (
+		<PageContainer>
+			<h1 className="text-2xl font-bold mb-4 mt-10 ml-35">Dashboard</h1>
+			<p>test</p>
+			{/* dashboard content */}
+		</PageContainer>
+	);
 }
